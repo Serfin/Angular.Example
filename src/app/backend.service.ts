4a03524e1ca3ca55cc97ce314af2d428c8d205de
fonter <serfin009@gmail.com>
2021-05-04 01:16:47 +0200

    added backend service

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, shareReplay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 import { IPost } from './post-list/IPost';
+import { IPostComment } from './post-list/IPostComment';
 import { IUser } from './user-list/IUser';
 
 @Injectable({
@@ -14,19 +16,56 @@ export class BackendService {
 
   getPosts(): Observable<IPost[]> {
     return this.client.get<IPost[]>(environment.backendUrl + '/posts')
-      .pipe();
+      .pipe(
+        shareReplay(),
+        catchError(err => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
   }
 
   getPostById(id: number): Observable<IPost> {
     return this.client.get<IPost>(environment.backendUrl + `/posts/${id}`)
-      .pipe();
+      .pipe(
+        shareReplay(),
+        catchError(err => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
+  }
+
+  getPostComments(postId: number): Observable<IPostComment[]> {
+    return this.client.get<IPostComment[]>(environment.backendUrl + `/posts/${postId}/comments`)
+      .pipe(
+        shareReplay(),
+        catchError(err => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
   }
 
   getUsers(): Observable<IUser[]> {
-    return this.client.get<IUser[]>(environment.backendUrl + '/users');
+    return this.client.get<IUser[]>(environment.backendUrl + '/users')
+      .pipe(
+        shareReplay(1),
+        catchError(err => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
   }
 
   getUserById(id: number): Observable<IUser> {
-    return this.client.get<IUser>(environment.backendUrl + `/user/${id}`);
+    return this.client.get<IUser>(environment.backendUrl + `/users/${id}`)
+      .pipe(
+        shareReplay(1),
+        catchError(err => {
+          console.error(err);
+          return EMPTY;
+        })
+      );
   }
 }
