ab81becee286116ecddb07bace5f752b64e511e1
fonter <serfin009@gmail.com>
2021-05-04 22:34:30 +0200

    added app routing, global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    UserListComponent
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([]),
+    RouterModule.forRoot([
+      { path: "home", component: HomeComponent },
+      { path: "posts", component: PostListComponent },
+      { path: "users", component: UserListComponent },
+      { path: "user/:id", component: UserDetailsComponent },
+      { path: "**", redirectTo: "/home", pathMatch: "full" },
+      { path: "", redirectTo: "/home", pathMatch: "full" }
+    ]),
   ],
   bootstrap: [AppComponent]
 })
