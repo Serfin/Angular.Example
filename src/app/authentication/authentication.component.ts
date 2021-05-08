253ec01f326665bcad25ff52bcc07742425ab5c4
fonter <serfin009@gmail.com>
2021-05-08 17:24:25 +0200

    added login error message

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthorizationService } from '../common/services/authorization.service';
+import { AuthenticationModel } from './authentication-model';
 
 @Component({
   selector: 'app-authentication',
@@ -8,16 +9,23 @@ import { AuthorizationService } from '../common/services/authorization.service';
   styleUrls: ['./authentication.component.scss']
 })
 export class AuthenticationComponent {
+  authModel: AuthenticationModel= {} as AuthenticationModel;
+  loginResult?: boolean;
+  loginErrorMessage!: string;
+  
   constructor(private authService: AuthorizationService,
     private router: Router) {
   }
 
-  login(login: string, password: string): void {
-    let result = this.authService
-      .login({ login: login, password: password });
+  onSubmit(): void {
+    let loginResult = this.authService
+      .login({ login: this.authModel?.login, 
+        password: this.authModel?.password });
 
-    if (result) {
+    if (loginResult) {
       this.router.navigateByUrl('/home');
+    } else {
+      this.loginErrorMessage = "Invalid login or passoword"
     }
   }
 }
